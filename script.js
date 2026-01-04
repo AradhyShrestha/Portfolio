0901c91716a1933779aeb83c955effeafccbb78c
Aradhy <aradhyshrestha1509@gmail.com>
2026-01-04 21:25:12 +0530

    Change slidesPerView to 3 for all breakpoints

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ var swiper = new Swiper(".mySwiper", {
     pagination: { el: ".swiper-pagination", clickable: true },
     navigation: { nextEl: ".custom-next", prevEl: ".custom-prev" },
     breakpoints: {
-      0: { slidesPerView: 1 },
-      576: { slidesPerView: 2 },
+      0: { slidesPerView: 3 },
+      576: { slidesPerView: 3 },
       992: { slidesPerView: 3 }
     }
   });
