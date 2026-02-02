f3b306bbf5a1e60a3a9b98926faa15859f9cdede
Aradhy <aradhyshrestha1509@gmail.com>
2026-02-02 20:18:25 +0530

    Adjust slidesPerView for responsive breakpoints

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ var swiper = new Swiper(".mySwiper", {
     pagination: { el: ".swiper-pagination", clickable: true },
     navigation: { nextEl: ".custom-next", prevEl: ".custom-prev" },
     breakpoints: {
-      0: { slidesPerView: 3 },
-      576: { slidesPerView: 3 },
+      0: { slidesPerView: 1 },
+      576: { slidesPerView: 2 },
       992: { slidesPerView: 3 }
     }
   });
