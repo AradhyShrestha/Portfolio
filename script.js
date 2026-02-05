f3493b6566fb83060caf1fa3042630543f9c999d
Aradhy <aradhyshrestha1509@gmail.com>
2026-02-05 20:50:16 +0530

    Refactor Swiper initialization for vertical direction
    Updated Swiper configuration to use vertical direction and simplified parameters.

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: { delay: 2500 },
-    pagination: { el: ".swiper-pagination", clickable: true },
-    navigation: { nextEl: ".custom-next", prevEl: ".custom-prev" },
-    breakpoints: {
-      0: { slidesPerView: 1 },
-      576: { slidesPerView: 2 },
-      992: { slidesPerView: 3 }
-    }
-  });
-  
+const swiper = new Swiper('.myswiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.custom-next',
+    prevEl: '.custom-prev',
+  },
+});
